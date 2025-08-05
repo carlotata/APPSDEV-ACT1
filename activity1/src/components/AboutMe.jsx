4f3c971ff0dfe2b26028fce3acf6afdf664e112f
Carl Aviso <avisojohn040@gmail.com>
2025-08-05 10:35:06 +0800

    proper spacing

--- activity1/src/components/AboutMe.jsx
+++ activity1/src/components/AboutMe.jsx
@@ -4,7 +4,10 @@ export default function AboutMe() {
    return (
       <div className="AboutMe">
          <h2>ABOUT ME</h2>
-            <p>I am John Carl Aviso, 22 years old and a BSIT-3C Student. Currently residing in Cordova Cebu</p>
+         <p>
+            I am John Carl Aviso, 22 years old and a BSIT-3C Student. Currently
+            residing in Cordova Cebu
+         </p>
       </div>
    );
 }
