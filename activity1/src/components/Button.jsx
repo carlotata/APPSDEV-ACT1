86ef66438a007412c6a379ff2730da0b719151a3
Carl Aviso <avisojohn040@gmail.com>
2025-08-06 21:03:52 +0800

    edited button to onclick function

--- activity1/src/components/Button.jsx
+++ activity1/src/components/Button.jsx
@@ -1,7 +1,7 @@
 import "../styles/Button.css";
-import fb from "../assets/fb.png"
-import instagram from "../assets/instagram.jpg"
-import dc from "../assets/dc.jpg"
+import fb from "../assets/fb.png";
+import instagram from "../assets/instagram.jpg";
+import dc from "../assets/dc.jpg";
 
 export default function Button() {
    return (
@@ -9,16 +9,27 @@ export default function Button() {
          <h2>REACH ME OUT</h2>
          <a href="https://www.facebook.com/jc.aviso.9/" target="blank">
             <img src={fb} alt="" className="logo" />
-            <button id="1">Facebook</button>
          </a>
          <a href="https://www.instagram.com/jcarl_012/" target="blank">
             <img src={instagram} alt="" className="logo" />
-            <button id="2">Instagram</button>
          </a>
          <a href="https://discord.com/users/761688402881151036" target="blank">
             <img src={dc} alt="" className="logo" />
-            <button id="3">Discord</button>
          </a>
+         <button
+            onClick={() => window.open("https://www.facebook.com/jc.aviso.9/")}>
+            Facebook
+         </button>
+         <button
+            onClick={() => window.open("https://www.instagram.com/jcarl_012/")}>
+            Instagram
+         </button>
+         <button
+            onClick={() =>
+               window.open("https://discord.com/users/761688402881151036")
+            }>
+            Discord
+         </button>
       </div>
    );
 }
